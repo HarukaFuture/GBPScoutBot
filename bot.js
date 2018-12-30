0b2ddcf283badadaf8b2b4d2d0a3befbd2efcb76
FutureHaruka <41966046+HarukaFuture@users.noreply.github.com>
2018-12-31 02:07:57 +0800

    Version 1.1.0

--- bot.js
+++ bot.js
@@ -7,72 +7,90 @@ const config = JSON.parse(fs.readFileSync('config.json','utf8'))
 const Telegraf = require('telegraf');
 const Telegram = require('telegraf/telegram');
 const request = require('superagent');
+var JsonDB = require('node-json-db');
+var crc32 = require('crc32');
 //定义区
 const bot = new Telegraf(config.apikey);
 require('superagent-cache')(request)
+var db = new JsonDB("serverdb", true, false);
 //END
-async function getcard (){
-	var card = await request.get(`https://api.bandori.ga/v1/${config.gameserver}/card?&sort=asc&orderKey=cardId`).forceUpdate(true)
+var gameserver = config.gameserver
+async function cardInit(){
+	let cardLstEN = {}
+	global.star = {}
+	global.cardCount = {}
+	global.cardLst = {}
+	global.resVersion = {}
+	cardLstEN.data = (await getcard('en')).data.filter((obj)=>{
+		let now = Date.now()
+		return obj.releasedAt < now
+	})
+	cardLstEN.totalCount = cardLstEN.data.length
+	
+	global.cardLst.jp = await getcard('jp')
+	global.cardLst.kr = await getcard('kr')
+	global.cardLst.tw = await getcard('tw')
+	global.cardLst.en = cardLstEN
+	
+	global.star.jp = JSON.parse(starRarity(global.cardLst.jp.data));
+	global.star.kr = JSON.parse(starRarity(global.cardLst.kr.data));
+	global.star.tw = JSON.parse(starRarity(global.cardLst.tw.data));
+	global.star.en = JSON.parse(starRarity(global.cardLst.en.data));
+
+	global.cardCount.jp = global.cardLst.jp.totalCount
+	global.cardCount.kr = global.cardLst.kr.totalCount
+	global.cardCount.tw = global.cardLst.tw.totalCount
+	global.cardCount.en = global.cardLst.en.totalCount
+	
+	global.resVersion.jp = await getResVer('jp')
+	global.resVersion.kr = await getResVer('kr')
+	global.resVersion.tw = await getResVer('tw')
+	global.resVersion.en = await getResVer('en')
+	fs.writeFileSync('star.json',JSON.stringify(global.cardLst.en))
+}
+async function getcard (gameserver){
+	var card = await request.get(`https://api.bandori.ga/v1/${gameserver}/card?&sort=asc&orderKey=cardId`).forceUpdate(true)
 	return card.body
-	}
-async function getresver (){
-	var resver = await request.get(`https://api.bandori.ga/v1/${config.gameserver}/version/res`).forceUpdate(true)
+}
+async function getResVer (gameserver){
+	var resver = await request.get(`https://api.bandori.ga/v1/${gameserver}/version/res`).forceUpdate(true)
 	return resver.text
 }
-async function init(){
-	var cardLst = await getcard()
-	star = JSON.parse(starRarity(cardLst))
-	console.log(cardLst.totalCount)
-	//命令区
-	bot.command('scout1', (ctx) => {scout1(ctx,star)});
-	bot.command('scout10', (ctx) => {scout10(ctx,star)});
-	return cardLst.totalCount
-}
 bot.telegram.getMe().then((botInfo) => {bot.options.username = botInfo.username})
-init()
+void async function(){
+	await cardInit()
+}()
+
 bot.command('ping',(ctx) => {ctx.reply('没炸!')});
+bot.command('setserver',(ctx) => {setOptServer(ctx)});
 bot.command('reinit',async (ctx) => {
 	if (config.admin.includes(ctx.message.from.id)){
-		var cardcount =await init()
-		var resver = await getresver()
-		ctx.reply(`Server Resources Version:${resver}
-卡牌数量:${cardcount}`,{'reply_to_message_id':ctx.message.message_id})
+		await cardInit()
+		ctx.reply(`Server Resources Version:${JSON.stringify(global.resVersion)}
+卡牌数量:${JSON.stringify(global.cardCount)}`,{'reply_to_message_id':ctx.message.message_id})
 	}else{
 		ctx.reply(`你没有权限`,{'reply_to_message_id':ctx.message.message_id})
 	}
 });
+bot.command('scout1', (ctx) => {scout1(ctx)});
+bot.command('scout10', (ctx) => {scout10(ctx)});
+bot.on('inline_query', (ctx) => {inlineScout(ctx);console.log(ctx.inlineQuery)})
 bot.startPolling()
+
+
 //中间件函数区
-async function scout1 (ctx,star){ //单抽!
-	const arr = [4,3,2]
-	var renindex = arr[parseInt(returnRandom()*arr.length)]
-	var card = star['star'+renindex][Math.floor((Math.random()*star['star'+renindex].length))]
-	var url = `https://bangdream.ga/card/${config.gameserver}/${card.cardId}/0`
-	var name = await characterName(card.characterId)
-	ctx.replyWithPhoto(`https://res.bandori.ga/assets-${config.gameserver}/characters/resourceset/${card.cardRes}_rip/card_normal.png`,{"caption":`${'★'.repeat(card.rarity)} [${card.title}](${url}) ${name}`,parse_mode:'Markdown','reply_to_message_id':ctx.message.message_id}).catch((err)=>{ctx.reply(`过于频繁!`,{'reply_to_message_id':ctx.message.message_id})})
+async function scout1 (ctx){ //单抽!
+	var scouts = await scout(1,getOptServer(ctx.message.chat.id))
+	ctx.replyWithPhoto(scouts[0].media,{"caption":scouts[0].caption,parse_mode:'Markdown','reply_to_message_id':ctx.message.message_id}).catch((err)=>{ctx.reply(`过于频繁!`,{'reply_to_message_id':ctx.message.message_id})})
 }
-async function scout10 (ctx,star){//抽抽抽抽抽抽抽抽抽抽!
-	const arr = [4,3,2]
-	var cardresult = []
-	for (i=0;i<10;i++){
-		var renindex = arr[parseInt(returnRandom()*arr.length)]
-		var card = star['star'+renindex][Math.floor((Math.random()*star['star'+renindex].length))]
-		var url = `https://bangdream.ga/card/${config.gameserver}/${card.cardId}/0`
-		var name = await characterName(card.characterId)
-		cardresult.push({
-			type:'photo',
-			media:`https://res.bandori.ga/assets-${config.gameserver}/characters/resourceset/${card.cardRes}_rip/card_normal.png`,
-			caption:`${'★'.repeat(card.rarity)} [${card.title}](${url}) ${name}`,
-			parse_mode:'Markdown'
-		})
-	}
-	console.log(cardresult)
-	ctx.replyWithMediaGroup(cardresult,{'reply_to_message_id':ctx.message.message_id}).catch((err)=>{ctx.reply(`过于频繁!`,{'reply_to_message_id':ctx.message.message_id})})
+async function scout10 (ctx){//抽抽抽抽抽抽抽抽抽抽!
+	var scouts = await scout(10,getOptServer(ctx.message.chat.id))
+	ctx.replyWithMediaGroup(scouts,{'reply_to_message_id':ctx.message.message_id}).catch((err)=>{ctx.reply(`过于频繁!`,{'reply_to_message_id':ctx.message.message_id})})
 }
 //基本函数
 function returnRandom(){ //随机数函数
-	var m = Math.random();
-	n = Math.random()/3
+	let m = Math.random();
+	let n = Math.random()/3
 	if(m < 0.03){
 		return 0+n;
 	}
@@ -83,20 +101,72 @@ function returnRandom(){ //随机数函数
 		return 2/3+n
 	}
 }
-function starRarity (cardLst){ //星级卡牌筛选
+function starRarity (data){ //星级卡牌筛选
 	var star2 = [];
 	var star3 = [];
 	var star4 = [];
-	for (i=0;i<cardLst.data.length;i++){
-		switch(cardLst.data[i].rarity){
-			case 2:star2.push(cardLst.data[i]);break;
-			case 3:star3.push(cardLst.data[i]);break;
-			case 4:star4.push(cardLst.data[i]);break;
+	for (i=0;i<data.length;i++){
+		switch(data[i].rarity){
+			case 2:star2.push(data[i]);break;
+			case 3:star3.push(data[i]);break;
+			case 4:star4.push(data[i]);break;
 		}
 	}
 	return JSON.stringify({star2,star3,star4});
 }
-async function characterName(cid){//角色ID
-	var chara = await request.get(`https://api.bandori.ga/v1/${config.gameserver}/chara/${cid}`) //获取卡牌列表
+async function characterName(cid,gameserver){//角色ID
+	var chara = await request.get(`https://api.bandori.ga/v1/${gameserver}/chara/${cid}`) //获取卡牌列表
 	return chara.body.characterName
+}
+async function scout(i,gameserver){
+	const arr = [4,3,2]
+	var result = []
+	for (var m=0;m<i;m++){
+		var renindex = arr[parseInt(returnRandom()*arr.length)]
+		var card = global.star[gameserver]['star'+renindex][Math.floor((Math.random()*global.star[gameserver]['star'+renindex].length))]
+		var url = `https://bangdream.ga/card/${gameserver}/${card.cardId}/0`
+		var name = await characterName(card.characterId,gameserver)
+		result.push({
+			type:'photo',
+			media:`https://res.bandori.ga/assets-${gameserver}/characters/resourceset/${card.cardRes}_rip/card_normal.png`,
+			caption:`${'★'.repeat(card.rarity)} [${card.title}](${url}) ${name}`,
+			parse_mode:'Markdown'
+		})
+	}
+	return result
+}
+function getOptServer(chatid){
+	try{
+		console.log(db.getData(`/set/${crc32(chatid.toString())}`))
+	}catch{
+		return config.gameserver
+	}
+	return db.getData(`/set/${crc32(chatid.toString())}`)
+}
+function setOptServer(ctx){
+	switch(ctx.message.text.substr(ctx.message.entities[0]['length']+1)){
+		case 'jp':db.push(`/set/${crc32(ctx.message.chat.id.toString())}`,'jp');break;
+		case 'kr':db.push(`/set/${crc32(ctx.message.chat.id.toString())}`,'kr');break;
+		case 'tw':db.push(`/set/${crc32(ctx.message.chat.id.toString())}`,'tw');break;
+		case 'en':db.push(`/set/${crc32(ctx.message.chat.id.toString())}`,'en');break;
+		default:
+		return ctx.replyWithMarkdown(`ja:Japan Server
+kr:Korea Server
+tw:RoC/Taiwan Server
+en:Intl. Server
+`,{'reply_to_message_id':ctx.message.message_id})
+	}
+	ctx.reply(`Data Lang:${db.getData('/set/'+(crc32(ctx.message.chat.id.toString())))}`,{'reply_to_message_id':ctx.message.message_id})
+}
+async function inlineScout(ctx){
+	var scouts = await scout(1,getOptServer(ctx.inlineQuery.from.id))
+	console.log(await ctx.answerInlineQuery([{
+		type:'photo',
+		id:'1',
+		photo_url:scouts[0].media,
+		thumb_url:'https://i.loli.net/2018/12/31/5c29080e36029.jpg',
+		caption:scouts[0].caption,
+		parse_mode:'Markdown',
+		title:'Scout one!'+'Data Lang:'+getOptServer(ctx.inlineQuery.from.id)
+	}],{cache_time:0,is_personal:true}))
 }
